2b5345b21340e5d393a7eb47558a591c94d3c216
Eli Sheets <eli@ideablock.io>
2019-08-28 20:28:44 -0400

    method improvements

--- bin/ideablock.js
+++ bin/ideablock.js
@@ -7,51 +7,49 @@ const off = require('../lib/off')
 const remove = require('../lib/remove')
 const help = require('../lib/help')
 const status = require('../lib/status')
-const log = console.log
 
 program
   .command('init')
   .alias('i')
   .description('Initiate automatic commit tethering to Bitcoin and Litecoin blockchains in the directory from which the command is called')
   .action(function () {
-    init()
+    init.init()
   })
 
 program
   .command('on')
   .description('Turn on automatic tethering of git commits to Bitcoin and Litecoin blockchains in directory from which the command is called.')
   .action(function () {
-    on()
+    on.on()
   })
 
 program
   .command('off')
   .description('Turn off automatic tethering of git commits to Bitcoin and Litecoin blockchains in the directory from which the command is called')
   .action(function () {
-    off()
+    off.off()
   })
 
-  program
+program
   .command('status')
   .alias('r')
   .description('Completely removes Ideablock Commit functionality in the directory from which the command is called, removes post-commit git hook, removes .ideablock directory from the repository')
   .action(function () {
-    status()
+    status.status()
   })
-
 
 program
   .command('remove')
   .alias('r')
   .description('Completely removes Ideablock Commit functionality in the directory from which the command is called, removes post-commit git hook, removes .ideablock directory from the repository')
   .action(function () {
-    remove()
+    remove.remove()
   })
 
 program
   .command('help')
   .action(function () {
-    help()
+    help.help()
   })
 
 program.parse(process.argv)
